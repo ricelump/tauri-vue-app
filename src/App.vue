3cfd6e5a015dc4e994e422429259a4160cfbdfeb
Rin <rin.git@preblocks.com>
2026-03-11 23:40:00 +0800

    chore: init

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { invoke } from "@tauri-apps/api/core";
+
+const greetMsg = ref("");
+const name = ref("");
+
+async function greet() {
+  // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+  greetMsg.value = await invoke("greet", { name: name.value });
+}
+</script>
+
+<template>
+  <main class="container">
+    <h1>Welcome to Tauri + Vue</h1>
+
+    <p>Click on the Tauri, Vite, and Vue logos to learn more.</p>
+
+    <form class="row" @submit.prevent="greet">
+      <input id="greet-input" v-model="name" placeholder="Enter a name..." />
+      <button type="submit">Greet</button>
+    </form>
+    <p>{{ greetMsg }}</p>
+  </main>
+</template>
