f77c77cc8300616a10b4968cba86e10033f8f28b
Rin <rin.git@preblocks.com>
2026-04-27 19:19:46 +0800

    feat: add cors fetch plugin

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -15,6 +15,7 @@ pub fn run() {
 		.plugin(tauri_plugin_store::Builder::new().build())
 		.plugin(tauri_plugin_dialog::init())
 		.plugin(tauri_plugin_opener::init())
+		.plugin(tauri_plugin_cors_fetch::init())
 		.invoke_handler(tauri::generate_handler![greet])
 		.run(tauri::generate_context!())
 		.expect("error while running tauri application");
